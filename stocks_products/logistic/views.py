cb023b94a8e2a9c88daa3852871236601c7452d4
Barynin Anton <118472119+Voronwe9@users.noreply.github.com>
2023-12-21 20:12:08 +0300

    Update views.py
    test 1

--- stocks_products/logistic/views.py
+++ stocks_products/logistic/views.py
@@ -24,4 +24,4 @@ class StockViewSet(ModelViewSet):
 
 class TestView(APIView):
     def get(self, request):
-        return Response({"message": "Всем большой привет!"})
+        return Response({"message": "Тест 1: ручная проверка!"})
