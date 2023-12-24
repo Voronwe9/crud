3a60f7f30dc1b49da9f7e91301145efd6a8573aa
Barynin Anton <antonbarynin@yandex.ru>
2023-12-24 16:39:08 +0300

    change tast3

--- stocks_products/logistic/views.py
+++ stocks_products/logistic/views.py
@@ -24,4 +24,4 @@ class StockViewSet(ModelViewSet):
 
 class TestView(APIView):
     def get(self, request):
-        return Response({"message": "Тест 2: ручная проверка!"})
+        return Response({"message": "Тест 3: ручная проверка!"})
